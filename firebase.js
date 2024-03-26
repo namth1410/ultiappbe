d4fe118668bcbd3f90f73a90163d2764280d80b4
20020450 <tranhainam1410@gmail.com>
2024-03-26 07:14:53 +0700

    fix: remove deploy firebase

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import config from "./config.js";
 
+
 const firebase = initializeApp(config.firebaseConfig);
 
 export const firestore = getFirestore(firebase);
