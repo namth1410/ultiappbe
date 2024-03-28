1054bc3f63b33232c0de2bbc025797661a465277
namth1410 <tranhainam1410@gmail.com>
2024-03-28 16:40:37 +0700

    feat: paging routine user

--- app.js
+++ app.js
@@ -29,6 +29,28 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.get("/getClasses", async (req, res) => {
+  try {
+    const q = query(collection(firestore, "classes"));
+    const querySnapshot = await getDocs(q);
+    const classes = [];
+    querySnapshot.forEach((doc) => {
+      classes.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+    if (classes.length > 0) {
+      res.status(200).json(classes);
+    } else {
+      res.status(404).send("Classes not found");
+    }
+  } catch (error) {
+    console.error("Error fetching classes:", error);
+    res.status(500).json({ error: "Error fetching classes" });
+  }
+});
+
 app.use("/", verifySessionCookieMiddleware);
 
 app.use("/classes", classRouter);
@@ -101,28 +123,6 @@ const checkCookie = (req, res, next) => {
     next();
   }
 };
-
-app.get("/getClasses", checkCookie, async (req, res) => {
-  try {
-    const q = query(collection(firestore, "classes"));
-    const querySnapshot = await getDocs(q);
-    const classes = [];
-    querySnapshot.forEach((doc) => {
-      classes.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-    if (classes.length > 0) {
-      res.status(200).json(classes);
-    } else {
-      res.status(404).send("Classes not found");
-    }
-  } catch (error) {
-    console.error("Error fetching classes:", error);
-    res.status(500).json({ error: "Error fetching classes" });
-  }
-});
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`)
